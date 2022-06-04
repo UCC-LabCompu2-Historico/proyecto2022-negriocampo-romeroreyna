84a73d059900a8b1b50460182e262f17ca16004c
mateonegri <mateonegriocampoaa@gmail.com>
2022-06-04 12:36:04 -0300

    Js nombre y tiro

---
+++ funciones.js
@@ -0,0 +1,26 @@
+// /**
+//  * Descripción
+//  * @method Nombre de la función
+//  * @param Parametro A
+//  * @param Parametro B
+//  * @return Valor que retorna
+//  */
+
+// function tomarDatos(){
+//     let nombre=localStorage.getItem("NombreDelUsuario");
+//     document.getElementById("asknombre").value=nombre;
+//     document.getElementById("aCompletar").innerHTML=nombre;
+// }
+//
+// function guardarNombre() {
+//     let nom;
+//     nom=localStorage.setItem("NombreDelUsuario", );
+//     window.open("jugar.html");
+// }
+//
+// function cargarLocalStorage() {
+//     let nom;
+//     nom=localStorage.getItem("NombreDelUsuario");
+//
+//     document.getElementById("Completar").value=nom;
+// }
